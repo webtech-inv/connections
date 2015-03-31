9a889a4971eedc666ad1308ce9445adf1a4199a8
amirbarkal <amirb@webtech-inv.co.il>
2015-04-01 01:43:53 +0300

    made some more shit

--- prepareLinuxOS.sh
+++ prepareLinuxOS.sh
@@ -5,5 +5,9 @@ sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/sysconfig/selinux
 
 umask 022
 
-echo "* - nofiles 8192" > /etc/security/limits.d/ibm.conf
+echo "* - nofile 8192" > /etc/security/limits.d/ibm.conf
 ulimit -n 8192
+
+yum install $(cat linuxLibraries.txt)
+
+mkdir -p /opt/IBM/IIM
