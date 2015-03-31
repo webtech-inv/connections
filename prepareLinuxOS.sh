6984851cc97f4ac2ae7b841ee4edf38411d1e402
amirbarkal <amirb@webtech-inv.co.il>
2015-04-01 00:42:51 +0300

    finished compiling library list

--- prepareLinuxOS.sh
+++ prepareLinuxOS.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/sysconfig/selinux
+
 umask 022
 
-echo "* - nofiles 8192" > /etc/security/limits.d/ibm2.conf
+echo "* - nofiles 8192" > /etc/security/limits.d/ibm.conf
 ulimit -n 8192
